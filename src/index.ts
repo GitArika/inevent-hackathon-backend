1d8063e7c9e69ad673d22e68c56ad42d24fd5b3a
Ariel Evangelista <ariel.evangelista@outlook.com>
2022-04-30 11:58:46 -0300

    chore: update application port

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.get('/healthCheck', (req, res) => {
     res.send({message: "Hello World"});
