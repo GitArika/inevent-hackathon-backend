688bc66d83b35fdcf7027debbe5e3017a3112192
Ariel Evangelista <ariel.evangelista@outlook.com>
2022-04-30 16:18:21 -0300

    chore: update production code

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,7 @@ import { User } from "../entitys/User";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "db-inevent-hackathon.codpu6zdd8lb.us-east-1.rds.amazonaws.com",
+    host: "aaq55exycirk3e.codpu6zdd8lb.us-east-1.rds.amazonaws.com",
     port: 3306,
     username: "admin",
     password: "adminadmin",
